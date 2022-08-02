98d1992df29c09c9a79a43e2d8103860639adf0c
Ilya <sia212007@gmail.com>
2022-08-02 15:45:48 +0300

    v1.1

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div style={{'fontSize': '20px', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'flex-direction': 'column'}}>
+        <div style={{'fontSize': '20px', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'flexDirection': 'column'}}>
             <div>Привет ! Меня зовут Илья и я создатель этого магазина. Этот сайт являеться проектом для портфолио, но вести его я буду регулярно</div>
             <div>В планах сделать BackEnd часть, сделать описание для товаров и много другое</div>
         </div>
--- src/components/News.js
+++ src/components/News.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const News = () => {
     return (
-        <div style={{'fontSize': '25px', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'}}>
-            Привет ! На данный момент новостей нет, но ты не расстраивайся ! Всё еще впереди !!!
+        <div style={{'fontSize': '25px', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center', 'textAlign': 'center'}}>
+            v1.1 - Добавлен индикатор активной страницы. Удалены предустановленные файлы React. {<br/>} Изменено иконку и название сайта
         </div>
     );
 };
